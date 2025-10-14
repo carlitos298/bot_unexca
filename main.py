27f61dff7ab55884206043215a60eea4a1d21ecd
carlitos298 <josueabrahammejiasmartines@gmail.com>
2025-10-14 11:37:48 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 from telegram import Update
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+from telegram.ext import Updater, CommandHandler, MessageHandler, filters, CallbackContext
 from flask import Flask
 
 # Información del centro universitario
